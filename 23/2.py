6b3b3a55cd190645ff86869e1ce5edc8aafb88ce
surbhitt <surbhit.thakur@gmail.com>
2023-12-04 18:56:47 +0530

    cleanup

--- 23/2.py
+++ 23/2.py
@@ -3,7 +3,6 @@ with open('2.in', 'r') as f:
     for line in f.readlines():
         um = {'r':0,'g':0,'b':0}
         game = line.split(': ')[1]
-        print(game)
         for c in range(len(game)):
             if game[c].isnumeric():
                 num = game[c]
@@ -12,16 +11,12 @@ with open('2.in', 'r') as f:
                     num+=game[c]
                     c+=1
                 um[game[c+1]] = max(um[game[c+1]], int(num))
-        print("um", um)
         allGames.append(um)
-print(allGames)
-
 
 ans = 0
 for i in range(len(allGames)):
     if allGames[i]['r'] <=12  and allGames[i]['g'] <= 13  and allGames[i]['b'] <=14:
-        print("this ", i+1)
         ans+=i+1
-print("___________", ans)
+print("ans =", ans)
 
 
